3b76e0ddb24355f04fdd6585892935d50f5b2d8a
Ankitjha05 <vastankit05@gmail.com>
2022-08-21 19:26:42 +0530

    updated code

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -3,34 +3,34 @@ import { PaginationButtonsList } from "./PaginationButtonsList";
 import { fetchPosts } from "../api/fetchPosts";
 
 const PostList = ({ limit }) => {
-    const [loader, setLoader] = useState(true);
-    const [page, setPage] = useState(1);
-    const [response, setResponse] = useState([]);
-    let increment = () => {
-        loaderfunc(page+1)
-        setPage(page + 1);
-        console.log('increment');
-    };
-    let loaderfunc = async (page) => {
-        setLoader(true);
-        console.log('before fetch')
-        let data =await fetchPosts(page, limit);
-        setResponse(data);
-        setLoader(false);
-        console.log('after fetch')
-    };
-    useEffect( () => {
-        console.log("before func call inside useeefect");
-        loaderfunc(page);
-        console.log('hello useeffect')
-    },[]);
-    return (
-        <>
-            {loader && <h4>loading...</h4> && console.log('inside return')}
-            {/* <Post lists={response}/> */}
-            {!loader && <PaginationButtonsList lists={response} page={page} increment={increment} />}
-        </>
-    );
+  const [loader, setLoader] = useState(true);
+  const [page, setPage] = useState(1);
+  const [response, setResponse] = useState([]);
+  let increment = () => {
+    loaderfunc(page + 1);
+    setPage(page + 1);
+  };
+  let loaderfunc = async (page) => {
+    setLoader(true);
+    let data = await fetchPosts(page, limit);
+    setResponse(data);
+    setLoader(false);
+  };
+  useEffect(() => {
+    loaderfunc(page);
+  }, []);
+  return (
+    <>
+      {loader && <h4>loading...</h4>}
+      {!loader && (
+        <PaginationButtonsList
+          lists={response}
+          page={page}
+          increment={increment}
+        />
+      )}
+    </>
+  );
 };
 
 export { PostList };
