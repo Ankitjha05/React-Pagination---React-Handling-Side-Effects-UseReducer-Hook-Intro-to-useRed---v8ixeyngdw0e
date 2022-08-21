ba63a8f2a7ec6bfad86ded9d030df170cb06ceaa
Ankitjha05 <vastankit05@gmail.com>
2022-08-21 19:20:43 +0530

    Updatd changes

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,29 +1,34 @@
 import React, { useState, useEffect } from "react";
 import { PaginationButtonsList } from "./PaginationButtonsList";
 import { fetchPosts } from "../api/fetchPosts";
-import { Post } from "./Post";
 
 const PostList = ({ limit }) => {
     const [loader, setLoader] = useState(true);
     const [page, setPage] = useState(1);
     const [response, setResponse] = useState([]);
     let increment = () => {
+        loaderfunc(page+1)
         setPage(page + 1);
+        console.log('increment');
     };
-    let loaderfunc = async () => {
+    let loaderfunc = async (page) => {
         setLoader(true);
+        console.log('before fetch')
         let data =await fetchPosts(page, limit);
         setResponse(data);
         setLoader(false);
+        console.log('after fetch')
     };
-    useEffect(() => {
-        loaderfunc();
-    });
+    useEffect( () => {
+        console.log("before func call inside useeefect");
+        loaderfunc(page);
+        console.log('hello useeffect')
+    },[]);
     return (
         <>
-            {loader && <h4>loading...</h4>}
+            {loader && <h4>loading...</h4> && console.log('inside return')}
             {/* <Post lists={response}/> */}
-            <PaginationButtonsList lists={response} page={page} increment={increment} />
+            {!loader && <PaginationButtonsList lists={response} page={page} increment={increment} />}
         </>
     );
 };
